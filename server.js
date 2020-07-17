a0fbd3b602e747bef664ff27b18f9375ff4aec01
ZuhaibAbdi <64994485+ZuhaibAbdi@users.noreply.github.com>
2020-07-17 14:51:58 -0600

    git init
    git

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const cors = require('cors')
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.DB_URI || 'mongodb://localhost/exercise-track', { useNewUrlParser: true });
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const apiRouter = require('./routes/api');
+app.use('/api/exercise', apiRouter);
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+  return next({status: 404, message: 'not found'});
 })
 
 // Error Handling middleware
@@ -29,20 +32,25 @@ app.use((err, req, res, next) => {
   let errCode, errMessage
 
   if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
+    // Mongoose validation error
+    errCode = 400; // Bad request
+    const keys = Object.keys(err.errors);
+    // Report the first validation error
+    errMessage = err.errors[keys[0]].message;
   } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+    // Generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
   }
   res.status(errCode).type('txt')
-    .send(errMessage)
-})
+    .send(errMessage);
+});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const portNum = process.env.PORT || 3000;
+
+// Listen for requests
+app.listen(portNum, () => {
+  console.log(`Listening on port ${portNum}`);
+});
+
+module.exports = app; // For testing
